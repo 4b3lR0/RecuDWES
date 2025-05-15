9b614127d9e84cb9a1e50c96e2f6295c1f4b8573
Abel Roca <arocac23@fpcoruna.afundacion.org>
2025-05-15 12:12:55 +0200

    settings y urls

--- RecuDWES/urls.py
+++ RecuDWES/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Recu import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('createBoss/', views.New_Boss),
 ]
